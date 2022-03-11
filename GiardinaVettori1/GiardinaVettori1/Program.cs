d308c8f658e1550d73aef889eaa3cce9fa57b45b
ale175 <alegia005@gmail.com>
2022-03-11 10:49:10 +0100

    Primi 2 esercizi svolti

---
+++ GiardinaVettori1/GiardinaVettori1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GiardinaVettori1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dichiarazione variabili
+            const int indice = 400;
+            int[] valori = new int[indice];
+            int valoreCasuale;
+            int i = 0;
+
+            //Codice del programma
+            Console.WriteLine("ESERCIZIO VETTORI 1");
+
+            for (i = 0; i < indice; i++)
+            {
+                Random generatore = new Random();
+                valoreCasuale = generatore.Next(1, 101);
+                valori[i] = valoreCasuale;
+                Console.WriteLine(valori[i]);
+            }
+        }
+    }
+}
---
+++ GiardinaVettori2/GiardinaVettori2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GiardinaVettori2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dichiarazione variabili
+            const int indice = 100;
+            int[] valori = new int[indice];
+            int valoreCasuale;
+            int i = 0;
+
+            //Codice del programma
+            Console.WriteLine("ESERCIZIO VETTORI 2");
+
+            for ( i = 0; i < indice; i++)
+            {
+                Random generatore = new Random();
+                valoreCasuale = generatore.Next(1, 101);
+                valori[i] = valoreCasuale;
+
+                if (valori[i] > 50)
+                {
+                    Console.WriteLine(valori[i]);
+                }
+            }
+
+        }
+    }
+}
